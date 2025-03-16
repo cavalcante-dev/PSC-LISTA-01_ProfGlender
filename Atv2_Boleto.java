658a4fc12181b546823bcf9091cb5e19dab1d9eb
cavalcante-html <leandrocavalcante2504@gmail.com>
2025-03-16 14:06:48 -0300

    Update2.1

--- Atv2_Boleto.java
+++ Atv2_Boleto.java
@@ -5,7 +5,7 @@ public class Atv2_Boleto {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        double boleto1, boleto2, boleto3, boleto4, totalBoletos;
+        double boleto1, boleto2, boleto3, boleto4, totalBoletos, salarioLiquido, salarioFinal;
 
         System.out.println("Digite seus boletos (R$): ");
         boleto1 = sc.nextDouble();
@@ -13,9 +13,14 @@ public class Atv2_Boleto {
         boleto3 = sc.nextDouble();
         boleto4 = sc.nextDouble();
 
+        System.out.println("Digite seu salário liquido (RS$): ");
+        salarioLiquido = sc.nextDouble();
+
         totalBoletos = boleto1 + boleto2 + boleto3 + boleto4;
+        salarioFinal = salarioLiquido - totalBoletos;
 
         System.out.printf("Seu total de boletos é: R$%.2f", totalBoletos);
+        System.out.printf("\nSeu salário final é: R$%.2f", salarioFinal);
 
         sc.close();
 
